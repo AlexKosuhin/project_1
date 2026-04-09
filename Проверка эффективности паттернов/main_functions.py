f17bd6cf31042147f27046bb5f1462c7f616ae31
Alex_Kosuhin <kosuhin1990@gmail.com>
2026-04-09 15:11:49 +0300

    Initial commit

---
+++ Проверка эффективности паттернов/main_functions.py
@@ -0,0 +1,494 @@
+import pandas as pd
+import numpy as np
+import scipy
+import statsmodels
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+# Преобразуем "сырой" датафрейм
+def good_dataframe(data, timeframe):
+  """Преобразует сырые рыночные данные в чистый DataFrame с правильными типами и индексом времени
+    
+    Подготавливает данные для технического анализа.
+    
+    Args:
+        data (pd.DataFrame): Исходный DataFrame с рыночными данными, содержащий столбцы:
+            ['<TICKER>', '<PER>', '<DATE>', '<TIME>', '<OPEN>', '<HIGH>', '<LOW>', '<CLOSE>', '<VOL>']
+            
+    Returns:
+        tuple: Возвращает кортеж из двух DataFrame:
+            - Основной DataFrame
+            - Копия DataFrame для безопасного резервирования
+            
+    Processing Logic:
+        1. Удаление избыточных столбцов
+        2. Переименование столбцов в human-friendly формат
+        3. Преобразование типов данных
+        4. Создание правильного временного индекса
+    
+    """
+  # 1. Делаем копию, чтобы не изменялся исходный датафрейм
+  data = data.copy()
+  
+  # 2. Переименовываем столбцы для удобства работы
+  data.columns = ['ticker', 'per', 'date', 'time', 'open', 'high', 'low', 'close', 'volume']
+    
+  # 3. Преобразуем дату из формата YYYYMMDD в datetime
+  data['date'] = pd.to_datetime(data['date'], format='%Y%m%d')
+    
+  # 4. Обрабатываем время (HHMMSS -> datetime.time)
+  data['time'] = pd.to_datetime(data['time'], format='%H%M%S').dt.time
+    
+  # 5. Комбинируем дату и время в единую метку времени
+  data['time'] = pd.to_datetime(
+        data['date'].astype('str') + ' ' + data['time'].astype('str'))
+    
+  # 6. Удаляем отдельный столбец даты (теперь он в индексе)
+  data.drop(['date'], inplace=True, axis=1)
+  
+  # 7. Установка индекса
+  data_final = data.set_index('time')
+  
+  
+  
+  def new_timeframe(data, timeframe):
+    """Преобразует минутные данные (1М) в указанный временной интервал, сохраняя структуру OHLCV-данных.
+    
+    Использует принципы агрегации свечных данных:
+    - Open - первое значение периода
+    - High - максимум периода
+    - Low - минимум периода
+    - Close - последнее значение периода
+    - Volume - сумма объема за период
+
+    Args:
+        data (pd.DataFrame): Исходный DataFrame с 1-минутными данными, 
+                            должен содержать колонки ['open', 'high', 'low', 'close', 'volume']
+                            и иметь DateTimeIndex
+        timeframe (str): Желаемый таймфрейм из списка доступных:
+                        ['5 min', '15 min', '30 min', '1h', '2h', '4h', 'D']
+
+    Returns:
+        pd.DataFrame: Новый DataFrame с преобразованными данными в указанном таймфрейме
+        
+    Raises:
+        ValueError: Если передан неподдерживаемый timeframe
+    """
+
+    dict_tf = {'5 min' : '5min', '15 min' : '15min', '30 min' : '30min',
+               '1h' : '1h', '2h' : '2h', '4h' : '4h', 'D' : 'D'}
+
+    return_data = data.resample(dict_tf[timeframe]).agg({
+            'ticker': 'first',
+            'per': 'first',
+            'open': 'first',
+            'high': 'max',
+            'low': 'min',
+            'close': 'last',
+            'volume': 'sum'
+        }).dropna()
+    
+
+    return_data['per'] = timeframe
+    return return_data
+  
+  result = new_timeframe(data_final, timeframe)
+  
+  result = result.reset_index()
+  
+  return result
+
+
+# Преобразуем датафрейм для удобства работы с 2 свечными паттернами
+def shift_features_2_candle(data):
+    """Смещает все основные столбцы на 1 период назад
+
+    Args:
+        data (pd.DataFrame): Исходный DataFrame с рыночными данными, содержащий столбцы:
+            ['time', 'ticker', 'per', 'open', 'high', 'low', 'close', 'volume']
+
+    Returns:
+        data (pd.DataFrame): Новый DataFrame с преобразованными данными, содержащий столбцы:
+        ['ticker', 'per', 'open_N', 'open_N-1', 'close_N', 'close_N-1', 'low_N',
+       'low_N-1', 'high_N', 'high_N-1', 'volume_N', 'volume_N-1', 'time_N',
+       'time_N-1']
+    """
+    data_c = data.copy()
+    for i in ['open', 'close', 'low', 'high', 'volume', 'time']:
+        data_c[f"{i}_N"] = data[i]
+        data_c[f'{i}_N-1'] = data[i].shift(1)
+    data_c.drop(['open', 'close', 'low', 'high', 'volume', 'time'], axis=1, inplace=True)
+    data_c.dropna(inplace=True)
+    return data_c
+
+
+
+
+# Размечаем датафрейм паттерном бычье поглощение
+def detection_bullish_engulfing_pattern(data):
+    """Функция для детекции бычьего паттерна поглощения
+
+    Args:
+        data (pd.DataFrame): Исходный DataFrame с рыночными данными, содержащий столбцы:
+        ['ticker', 'per', 'open_N', 'open_N-1', 'close_N', 'close_N-1', 'low_N',
+       'low_N-1', 'high_N', 'high_N-1', 'volume_N', 'volume_N-1', 'time_N',
+       'time_N-1']
+
+    Returns:
+        data (pd.DataFrame): Исходный DataFrame вместе с дополнительными 3 столбцами:
+        - pattern : 1 - 2 свечи наблюдения образуют паттерн, 0 - паттетна нет.
+        - signal : 1 - на предыдущей свече был паттерн, 0 - паттерна не было, сигнала на покупку на данной свече нет
+        - strategy : Название стратегии - 'bullish_engulfing_pattern'
+        
+    """
+
+    data = data.copy()
+    data['pattern'] = 0
+    data['signal'] = 0
+    data['strategy'] = 'bullish_engulfing_pattern'
+    
+    # Векторизованные вычисления
+    close_N = data['close_N']
+    close_N_1 = data['close_N-1']
+    open_N = data['open_N']
+    open_N_1 = data['open_N-1']
+    
+    body_N = data['close_N'] - data['open_N']
+    body_N_1 = data['close_N-1'] - data['open_N-1']
+    
+    # Базовое условие бычьего поглощения
+    base_condition = (
+        (body_N_1 < 0) & 
+        (body_N > 0) &
+        (open_N_1 < close_N) & 
+        (close_N_1 > open_N)
+    )
+    
+    # Отмечаем 2 свечи паттерна
+    pattern_mask = base_condition
+    data.loc[pattern_mask, 'pattern'] = 1
+    # Сигнал - следующая свеча после завершения паттерна
+    data.loc[pattern_mask.shift(1).fillna(False), 'signal'] = 1
+    
+    return data
+
+
+# Паттерн бычье харами
+def detection_bullish_harami_pattern(data):
+    """
+    Функция для детекции бычьего паттерна 'харами' (Bullish Harami) на ценовых данныx.
+    Паттерн Харами состоит из большой медвежьей свечи, за которой следует маленькая бычья свеча,
+    полностью содержащаяся в пределах тела предыдущей свечи.
+    Args:
+        data (pd.DataFrame): Исходный DataFrame с рыночными данными, содержащий столбцы:
+        ['ticker', 'per', 'open_N', 'open_N-1', 'close_N', 'close_N-1', 'low_N',
+       'low_N-1', 'high_N', 'high_N-1', 'volume_N', 'volume_N-1', 'time_N',
+       'time_N-1']
+
+    Returns:
+        data (pd.DataFrame): Исходный DataFrame вместе с дополнительными 3 столбцами:
+        - pattern : 1 - 2 свечи наблюдения образуют паттерн, 0 - паттетна нет.
+        - signal : 1 - на предыдущей свече был паттерн, 0 - паттерна не было, сигнала на покупку на данной свече нет
+        - strategy : Название стратегии - 'bullish_harami_pattern'
+        
+    """
+    data = data.copy()
+    data['pattern'] = 0
+    data['signal'] = 0
+    data['strategy'] = 'bullish_harami_pattern'
+    
+    # Векторизованные вычисления
+    close_N = data['close_N']
+    close_N_1 = data['close_N-1']
+    open_N = data['open_N']
+    open_N_1 = data['open_N-1']
+    
+    body_N = data['close_N'] - data['open_N']
+    body_N_1 = data['close_N-1'] - data['open_N-1']
+    
+    # Базовое условие для бычьего харами
+    base_condition = (
+        (body_N_1 < 0) & 
+        (body_N > 0) &
+        (close_N_1 < open_N) & 
+        (open_N_1 > close_N))
+    
+    # Отмечаем 2 свечи паттерна
+    pattern_mask = base_condition
+    data.loc[pattern_mask, 'pattern'] = 1
+    # Сигнал - следующая свеча после завершения паттерна
+    data.loc[pattern_mask.shift(1).fillna(False), 'signal'] = 1
+        
+    
+    return data
+
+
+# Паттерн просвет в облаках
+def detection_break_in_the_clouds(data):
+    """
+    Свечная модель, состоящая из 2 свечей. Первая свеча падающая, 
+    вторая свеча открывается ниже закрытия предыдущей и закрывается по цене, 
+    которая превосходит середину предыдущей свечи.
+    
+    Args:
+        data (pd.DataFrame): Исходный DataFrame с рыночными данными, содержащий столбцы:
+        ['ticker', 'per', 'open_N', 'open_N-1', 'close_N', 'close_N-1', 'low_N',
+       'low_N-1', 'high_N', 'high_N-1', 'volume_N', 'volume_N-1', 'time_N',
+       'time_N-1']
+
+    Returns:
+        data (pd.DataFrame): Исходный DataFrame вместе с дополнительными 3 столбцами:
+        - pattern : 1 - 2 свечи наблюдения образуют паттерн, 0 - паттетна нет.
+        - signal : 1 - на предыдущей свече был паттерн, 0 - паттерна не было, сигнала на покупку на данной свече нет
+        - strategy : Название стратегии - 'break_in_the_clouds'
+        
+    """
+    data = data.copy()
+    data['pattern'] = 0
+    data['signal'] = 0
+    data['strategy'] = 'break_in_the_clouds'
+    
+    # Векторизованные вычисления
+    close_N = data['close_N']
+    close_N_1 = data['close_N-1']
+    open_N = data['open_N']
+    open_N_1 = data['open_N-1']
+    
+    body_N = data['close_N'] - data['open_N']
+    body_N_1 = data['close_N-1'] - data['open_N-1']
+    center = (open_N_1 + close_N_1) / 2
+    
+    # Базовое условие для просвета в облаках
+    base_condition = (
+        (body_N_1 < 0) & 
+        (body_N > 0) &
+        (open_N < close_N_1) & 
+        (close_N < open_N_1) &
+        (close_N > center))
+    
+    # Отмечаем 2 свечи паттерна
+    pattern_mask = base_condition
+    data.loc[pattern_mask, 'pattern'] = 1
+    # Сигнал - следующая свеча после завершения паттерна
+    data.loc[pattern_mask.shift(1).fillna(False), 'signal'] = 1
+        
+    
+    return data
+
+
+# Паттерн бычья контратака
+def detection_bullish_counterattack(data):
+    """
+    Свечная модель, состоящая из 2 свечей. Первая свеча падающая, вторая свеча растущая.
+    Цены закрытия равны или очень близки друг к другу.
+    
+    Args:
+        data (pd.DataFrame): Исходный DataFrame с рыночными данными, содержащий столбцы:
+        ['ticker', 'per', 'open_N', 'open_N-1', 'close_N', 'close_N-1', 'low_N',
+       'low_N-1', 'high_N', 'high_N-1', 'volume_N', 'volume_N-1', 'time_N',
+       'time_N-1']
+
+    Returns:
+        data (pd.DataFrame): Исходный DataFrame вместе с дополнительными 3 столбцами:
+        - pattern : 1 - 2 свечи наблюдения образуют паттерн, 0 - паттетна нет.
+        - signal : 1 - на предыдущей свече был паттерн, 0 - паттерна не было, сигнала на покупку на данной свече нет
+        - strategy : Название стратегии - 'bullish_counterattack'
+        
+    """
+    data = data.copy()
+    data['pattern'] = 0
+    data['signal'] = 0
+    data['strategy'] = 'bullish_counterattack'
+    
+    # Векторизованные вычисления
+    close_N = data['close_N']
+    close_N_1 = data['close_N-1']
+    
+    body_N = data['close_N'] - data['open_N']
+    body_N_1 = data['close_N-1'] - data['open_N-1']
+    
+
+    # Базовое условие для просвета в облаках
+    base_condition = (
+        (body_N_1 < 0) & 
+        (body_N > 0) &
+        ((np.abs(close_N_1 - close_N) / close_N) * 100 <= 0.05))
+    
+    # Отмечаем 2 свечи паттерна
+    pattern_mask = base_condition
+    data.loc[pattern_mask, 'pattern'] = 1
+    # Сигнал - следующая свеча после завершения паттерна
+    data.loc[pattern_mask.shift(1).fillna(False), 'signal'] = 1
+        
+    
+    return data
+
+
+
+# Специальная функция для проверки эффективности паттернов 
+def filter_min_distance(data, column, N):
+    """Некоторые паттерны расположены слишком близко друг к другу, чтобы не было наложения сделок, 
+    необходимо удалить все сигналы на покупку, которые появляются во время удержания позиции.
+    Время удержания N периодов
+
+    Args:
+        data (pd.DataFrame): Преобразованный датафрейм
+        'signal' (int64): Сигнальный столбец.
+        N (int): Количество периодов, которые мы собираемся удерживать открытую позицию
+
+    Returns:
+        data (pd.DataFrame): Исходный датафрейм с отредактированным сигнальным столбцом
+    """
+    data = data.copy()
+    # Получаем все индексы наблюдений где у нас есть сигнал на покупку
+    one_index = data[data[column] == 1].index 
+    # Получаем разность между этими индексами
+    distances = np.diff(one_index)
+    # Маска для каждого сигнала. Первый сигнал на покупку всегда True
+    mask = [True]
+    
+    # Перебираем все разности между индексами
+    for dist in distances:
+        if dist >= N:
+            # Если разность между индексами больше N, то все хорошо.
+            # Мы добавляем эту сделку в список
+            mask.append(True)
+        else:
+            # Если расстояние меньше, то сделку игнорируем
+            mask.append(False)
+            
+    # Данные для удаления
+    to_remove = one_index[np.where(~np.array(mask))[0]]
+    data.loc[to_remove, column] = 0
+    
+    return data
+
+
+
+# Готовим данные для дальнейшей проверки
+def data_prepare_for_statistics(data, commission=0.0002):
+    """Функция предназначена для обработки результатов стратегий.
+
+    Args:
+        data (pd.DataFrame): на вход подается размеченный датафрейм с колонкой 'signal'
+        commission (float, optional): Размер комисии в долях процента (0.0002 ~ 0.02%). Defaults to 0.0002.
+
+    Returns:
+        list: Возвращаем список с датафреймами, каждый датафрейм это данные по каждой сделке (дата и размер прибыли в %)
+    """
+    data = data.copy()
+    # Удалим сперва все отметки паттерна и сигнала с последних 51 строки
+    data.loc[len(data) - 51 : len(data), ['pattern', 'signal']] = 0
+    
+    # Проходимся циклом по периодам удержания позиции
+    all_strategy = []
+    for N in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 15, 20, 25, 30, 35, 40, 45, 50]:
+        data_h = filter_min_distance(data, 'signal', N) # Получаем фильтрованные данные
+        data_h = data_h.set_index('time_N')
+        # Блок для определения цены входа и выхода
+        # То есть для каждой стратегии мы определяем вектора цен открытий и закрытий
+        # Так как удерживаем позицию мы N периодов, то в качестве всех кандидатов, 
+        # на которых мы можем закрыть позицию берем цены открытия через N периодов
+        exit_candidate = data_h['open_N'].shift(-N) 
+        is_entry = data_h['signal'] == 1
+        entry_price = data_h['open_N'][is_entry]
+        exit_price = exit_candidate[is_entry]
+        
+        # Считаем результат
+        gross_profit = exit_price.values - entry_price.values
+        commissions = exit_price.values * commission + entry_price.values * commission
+        net_profit = gross_profit - commissions
+        percent_profit_net = pd.Series(np.round(net_profit / entry_price.values, 5), index=entry_price.index,
+                               name='profit_percent') # % доход за сделку
+        percent_profit_without_commission = pd.Series(np.round(gross_profit / entry_price.values, 5), index=entry_price.index,
+                               name='gross_profit_percent') # % доход за сделку грязный
+        
+        strategy_data = pd.DataFrame({'profit_percent_net' : np.array(percent_profit_net), 
+                                      'percent_profit_without_commission' : np.array(percent_profit_without_commission),
+                                       'N' : N, 
+                                       'strategy_name' : data_h['strategy'].iloc[0],
+                                       'ticker' : data_h['ticker'].iloc[0],
+                                       'period' : data_h['per'].iloc[0]},
+                            index=percent_profit_net.index)
+        all_strategy.append(strategy_data)
+        
+    return all_strategy
+
+
+
+# Функция для получения результатов стратегии
+def trade_statistics_bull(all_strategy_data):
+    """Функция предназначена для агрегирования результатов стратегий по основным финансовым метрикам
+
+    Args:
+        all_strategy_data (list): Подаётся список с датафреймами, где для для каждой сделки установлено время и размер прибыли в %.
+
+    Returns:
+        data (pd.DataFrame): Возвращаем новый датафрейм со всеми необходимыми метриками для оценки стратегий.
+    """
+    capital = 100_000
+    strategies = []
+    for strategy in all_strategy_data:
+        
+        # Считаем основные метрики
+        ticker = strategy['ticker'].iloc[0]
+        strategy_name = strategy['strategy_name'].iloc[0]
+        period = strategy['period'].iloc[0]
+        N = strategy['N'].iloc[0]
+        count_of_trades = len(np.array(strategy['profit_percent_net']))
+        profit_percent_net = strategy['profit_percent_net']
+        percent_profit_without_commission = strategy['percent_profit_without_commission']
+        
+        # Кривая капитала 
+        capital_curve_net = capital * np.cumprod(1 + strategy['profit_percent_net'])
+        capital_curve_without_commission = capital * np.cumprod(1 + strategy['percent_profit_without_commission'])
+        
+        # Профит фактор
+        pr_per_net = np.array(strategy['profit_percent_net'])
+        profit_factor = np.round(np.sum(pr_per_net[pr_per_net > 0]) / np.abs(np.sum(pr_per_net[pr_per_net < 0])), 3)
+        
+        # Общий % доход !!!
+        total_per_return_r = np.array(np.cumprod(1 + strategy['profit_percent_net']))[-1]
+        total_per_return = np.round((total_per_return_r - 1) * 100, 2)
+        
+        # Win_rate 
+        win_rate = np.round(np.mean(pr_per_net > 0), 2)
+        
+        # R2
+        X = np.array(list(range(len(capital_curve_net)))).reshape(-1, 1)
+        y = np.array(capital_curve_net)
+        lin = LinearRegression()
+        lin.fit(X, y)
+        y_pred = lin.predict(X)
+        r2 = np.round(metrics.r2_score(y, y_pred), 2)
+        
+        # Средняя убыточная и прибыльная сделка 
+        mean_profit_trade = np.round(np.mean(pr_per_net[pr_per_net > 0]), 3)
+        mean_loss_trade = np.round(np.mean(pr_per_net[pr_per_net < 0]), 3)
+        avg_profit_loss_ratio = np.round(mean_profit_trade / mean_loss_trade, 3)
+        
+        # Максимальная просадка
+        cumsum_profit = np.cumsum(pr_per_net)
+        cummax_profit = np.maximum.accumulate(cumsum_profit)
+        max_drawdown = np.max(cummax_profit - cumsum_profit)
+        
+        strategies.append({
+          'ticker' : ticker,
+          'strategy name' : strategy_name,
+          'period' : period,
+          'N' : N,
+          'count of trades' : count_of_trades,
+          'profit factor' : profit_factor,
+          'total percent return' : total_per_return,
+          'R2' : r2,
+          'win_rate' : win_rate,
+          'mean profit (%)' : mean_profit_trade * 100,
+          'mean loss (%)' : mean_loss_trade * 100,
+          'profit/loss ratio' : np.round(np.abs(avg_profit_loss_ratio), 2),
+          'max drawdown (%)' : np.round(max_drawdown * 100, 1),
+          'capital curve net' : capital_curve_net,
+          'capital curve without commission' : capital_curve_without_commission,
+          'profit percent net' : profit_percent_net,
+          'percent profit without commission' : percent_profit_without_commission}
+          )
+        
+    return pd.DataFrame(strategies)
